3e8edf9fb81203211b15f3a5eaac5f37ceb26378
Jeff Yates <jeff@somewhatabstract.com>
2019-09-10 15:46:23 -0500

    Add some more examples

---
+++ __examples__/directory_target/example.js
@@ -0,0 +1,6 @@
+// This is a a javascript (or similar language) file
+
+// sync-start:directory_target ./a_directory
+const someCode = "does a thing";
+console.log(someCode);
+// sync-end:directory_target
