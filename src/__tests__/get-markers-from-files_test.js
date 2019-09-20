c244f2e25a0ee315ff488f0bc8bfedb6950d5490
Jeff Yates <jeff@somewhatabstract.com>
2019-09-20 13:20:39 -0500

    Add test for symlink-related behavior

--- src/__tests__/get-markers-from-files_test.js
+++ src/__tests__/get-markers-from-files_test.js
@@ -5,6 +5,7 @@ import getMarkersFromFiles from "../get-markers-from-files.js";
 import * as ParseFile from "../parse-file.js";
 import Logger from "../logger.js";
 import * as Ancesdir from "ancesdir";
+import * as CloneAsFixable from "../clone-as-unfixable.js";
 
 import type {Options} from "../types.js";
 
@@ -278,4 +279,49 @@ describe("#fromFiles", () => {
             expect.any(Function),
         );
     });
+
+    it("should clone existing file markers if the target of a symlink is already parsed", async () => {
+        // Arrange
+        jest.spyOn(ParseFile, "default").mockImplementation(
+            (file, fixable, comments, logger, logCb) => {
+                if (file === "a.js") {
+                    logCb("b.js");
+                    logCb("c.js");
+                }
+                return Promise.resolve(file);
+            },
+        );
+        jest.spyOn(fs, "realpathSync").mockImplementation(a => {
+            if (a === "c.js") {
+                return "a.js";
+            }
+            return a;
+        });
+        jest.spyOn(fs, "existsSync").mockReturnValue(true);
+        jest.spyOn(fs, "lstatSync").mockReturnValue({isFile: () => true});
+        jest.spyOn(path, "join").mockImplementation((a, b) => b);
+        jest.spyOn(path, "normalize").mockImplementation(b => b);
+        const cloneSpy = jest.spyOn(CloneAsFixable, "default");
+        const options: Options = {
+            includeGlobs: ["a.js", "b.js"],
+            comments: ["//"],
+            autoFix: true,
+            rootMarker: null,
+            dryRun: false,
+            excludeGlobs: [],
+        };
+
+        // Act
+        await getMarkersFromFiles(options, ["a.js", "b.js"], NullLogger);
+
+        // Assert
+        expect(cloneSpy).toHaveBeenCalledWith({
+            aliases: ["a.js", "c.js"],
+            markers: "a.js",
+        });
+    });
+
+    it("should clone symlink parsed file markers to real target", () => {});
+
+    it("should log an error if the file doesn't exist", () => {});
 });
