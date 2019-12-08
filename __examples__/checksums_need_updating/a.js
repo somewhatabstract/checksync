d75d1129eaea0e908776ab8c7c2cb25eb0d13570
Jeff Yates <jeff@somewhatabstract.com>
2019-12-07 21:50:10 -0600

    Update basic example to include indentation

--- __examples__/checksums_need_updating/a.js
+++ __examples__/checksums_need_updating/a.js
@@ -1,6 +1,8 @@
 // This is a a javascript (or similar language) file
 
-// sync-start:update_me 45678 __examples__/checksums_need_updating/b.py
-const someCode = "does a thing";
-console.log(someCode);
-// sync-end:update_me
+export default function test() {
+    // sync-start:update_me 45678 __examples__/checksums_need_updating/b.py
+    const someCode = "does a thing";
+    console.log(someCode);
+    // sync-end:update_me
+}
