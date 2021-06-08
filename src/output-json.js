5581ba115e7ab9b82fff820c285054355c9d0a5a
Kevin Barabash <kevinb@khanacademy.org>
2021-06-08 15:53:48 -0400

    normalize file paths in JSON items before passing them to JSON.stringify

--- src/output-json.js
+++ src/output-json.js
@@ -1,4 +1,7 @@
 // @flow
+import path from "path";
+import escapeRegExp from "lodash/escapeRegExp";
+
 import getLaunchString from "./get-launch-string.js";
 import ErrorCodes from "./error-codes.js";
 import {version} from "../package.json";
@@ -12,12 +15,34 @@ const outputJson = (
     jsonItems: Array<JsonItem>,
     violationFileNames: Array<string>,
 ): ErrorCode => {
+    const regex = new RegExp(escapeRegExp(path.sep), "g");
+    const normalize = (snippet: string): string => snippet.replace(regex, "/");
+
+    const normalizedJsonItems = jsonItems.map((item: JsonItem): JsonItem => {
+        if (item.type === "error") {
+            return {
+                ...item,
+                sourceFile: normalize(item.sourceFile),
+                targetFile: normalize(item.targetFile),
+                message: normalize(item.message),
+            };
+        } else {
+            return {
+                ...item,
+                sourceFile: normalize(item.sourceFile),
+                targetFile: normalize(item.targetFile),
+                message: normalize(item.message),
+                fix: item.fix && normalize(item.fix),
+            };
+        }
+    });
+
     log.log(
         JSON.stringify(
             {
                 version: version,
                 launchString: getLaunchString(),
-                items: jsonItems,
+                items: normalizedJsonItems,
             },
             null,
             4,
