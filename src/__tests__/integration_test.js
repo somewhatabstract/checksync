5494e827856a9d4fcea03c60dd3f868bca28fc1f
Jeff Yates <jeff@khanacademy.org>
2021-07-24 21:15:57 -0500

    Add ignore file example
    This adds the ignore file example, which doesn't work yet, and updates the integration tests to run with __examples__ as the working directory

--- src/__tests__/integration_test.js
+++ src/__tests__/integration_test.js
@@ -2,51 +2,34 @@
 import fs from "fs";
 import path from "path";
 import ancesdir from "ancesdir";
-import escapeRegExp from "lodash/escapeRegExp";
 import StringLogger from "../string-logger.js";
 
 import checkSync from "../check-sync.js";
 
 jest.mock("../get-launch-string.js", () => () => "checksync");
 
-describe("Integration Tests", () => {
+describe("Integration Tests (see __examples__ folder)", () => {
     beforeEach(() => {
         // Fast Glob will hang if we don't do this.
         jest.useRealTimers();
     });
 
-    const getExampleGlobs = () => {
-        const __examples__ = path.join(ancesdir(), "__examples__");
-        return (
-            fs
-                .readdirSync(__examples__)
-                /**
-                 * The whole symlink test is not going to work right on windows
-                 * so let's just skip it.
-                 */
-                .filter(
-                    (p) =>
-                        !(
-                            process.platform === "win32" &&
-                            p.includes("symlink")
-                        ),
-                )
-                .map((name) => [name, path.join(__examples__, name)])
-                .filter(([_, dirPath]) => fs.lstatSync(dirPath).isDirectory())
-                // Globs use forward slashes and we need to strip off the root
-                // to make sure this works for Windows.
-                .map(([name, dirPath]) => [
-                    name,
-                    dirPath
-                        .replace(ancesdir(), ".")
-                        .replace(new RegExp(escapeRegExp(path.sep), "g"), "/"),
-                ])
-                // Finally, this has to be an actual glob, or it won't work.
-                .map(([name, dirPath]) => [name, `${dirPath}/**`])
-                .sort()
-        );
-    };
-    const exampleGlobs = getExampleGlobs();
+    // Determine the examples folder path and set that as the working directory
+    const __examples__ = path.join(ancesdir(), "__examples__");
+    process.chdir(__examples__);
+
+    // Iterate over the __examples__ folders and determine eaches glob pattern.
+    const exampleGlobs = fs
+        .readdirSync(__examples__)
+        /**
+         * The whole symlink test is not going to work right on windows
+         * so let's just skip it.
+         */
+        .filter((p) => !(process.platform === "win32" && p.includes("symlink")))
+        .filter((name) => fs.lstatSync(name).isDirectory())
+        // Finally, this has to be an actual glob, or it won't work.
+        .map((name) => [name, `${name}/**`])
+        .sort();
 
     it.each(exampleGlobs)(
         "should report example %s to match snapshot",
