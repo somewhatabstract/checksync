4fd7057e82da949f70da9f9630232eda92e5fa50
Jeff Yates <jeff@somewhatabstract.com>
2025-02-26 17:31:04 -0600

    [fei6223.1.selftags] use line exact URLs in examples just to set a good example

--- __examples__/remote_tags_broken/example.jsx
+++ __examples__/remote_tags_broken/example.jsx
@@ -1,6 +1,6 @@
 const component = () => (
     <div>
-        {/* sync-start:remote_target_self_hash 840225308 https://github.com/somewhatabstract/checksync/blob/117f7e2c68e06847af333ed7a688d7f224cee374/__examples__/correct_checksums/a.js */}
+        {/* sync-start:remote_target_self_hash 840225308 https://github.com/somewhatabstract/checksync/blob/b9433f22ed9cfdf6c79edfd7254670ff53f82f7c/__examples__/remote_tags_match/example.jsx#L3 */}
         <span>Example!</span>
         {/* sync-end:remote_target_self_hash */}
     </div>
