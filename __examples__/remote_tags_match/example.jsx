1fef56b0998870e2685c91cf057ebfc839734a7a
Jeff Yates <jeff@somewhatabstract.com>
2025-02-26 17:25:26 -0600

    [fei6223.1.selftags] Add examples for broken and working remote tags

---
+++ __examples__/remote_tags_match/example.jsx
@@ -0,0 +1,9 @@
+const component = () => (
+    <div>
+        {/* sync-start:remote_target_self_hash 1153645584 https://github.com/somewhatabstract/checksync/blob/117f7e2c68e06847af333ed7a688d7f224cee374/__examples__/correct_checksums/a.js */}
+        <span>Example!</span>
+        {/* sync-end:remote_target_self_hash */}
+    </div>
+);
+
+export default component;
