7e89e84ec586c2c9a8ecb6136921c8f7c6c0ba23
Jeff Yates <jeff@somewhatabstract.com>
2025-03-04 18:16:07 -0600

    [somewhatabstract/issue2036] Update example to have a checksum to replace

--- __examples__/migrate_missing_target/example.js
+++ __examples__/migrate_missing_target/example.js
@@ -1,6 +1,6 @@
 // This is a a javascript (or similar language) file
 
-// sync-start:migrate_missing_target __examples__/migrate_missing_target/missing_target.py
+// sync-start:migrate_missing_target 12345 __examples__/migrate_missing_target/missing_target.py
 const someCode = "does a thing";
 console.log(someCode);
 // sync-end:migrate_missing_target
